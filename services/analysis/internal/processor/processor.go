eba4c41bf83e2e36ef653a1172cb0aeb6518fbd2
David Botos <dbotos@umich.edu>
2024-12-31 17:16:49 -0800

    identify what tables may be impacted by the transcripts data

--- services/analysis/internal/processor/processor.go
+++ services/analysis/internal/processor/processor.go
@@ -1,11 +1,26 @@
 package processor
 
 import (
-    "fmt"
-    "github.com/david-botos/BearHug/services/analysis/internal/types"
+	"fmt"
+	"os"
+
+	"github.com/david-botos/BearHug/services/analysis/internal/types"
 )
 
-func ProcessTranscript(params types.ProcessTranscriptParams) error {
-    // TODO: Implement transcript processing logic
-    return fmt.Errorf("ProcessTranscript not implemented")
+// TODO: Check the return types based on what i end up wanting to return, right now I assume ill just return true if its successful
+func ProcessTranscript(params types.ProcessTranscriptParams) (bool, error) {
+	prompt := GenerateTriagePrompt(params.Transcript)
+	schema := NewTriageSchema()
+	client := NewClient(os.Getenv("ANTHROPIC_API_KEY"))
+	result, err := client.RunClaudeInference(TriagePromptParams{prompt, schema})
+
+	// TODO: based on the tables identified that could be affected by the transcript, launch a goroutine to export to each standardized output
+	// TODO: erase ts below i just dont like the color red when im pushing
+	if err != nil {
+		return false, fmt.Errorf("error reading response: %w", err)
+	}
+	if result != nil {
+		return true, nil
+	}
+	return true, nil
 }

4fbab85bd54b7ea85d27b090f97a690925c6815f
David Botos <dbotos@umich.edu>
2025-01-12 18:32:29 -0800

    handler to store new validated objs

---
+++ services/analysis/internal/processor/validation/storeValidatedDetails.go
@@ -0,0 +1,23 @@
+package validation
+
+import (
+	"github.com/david-botos/BearHug/services/analysis/internal/processor/structOutputs"
+	"github.com/david-botos/BearHug/services/analysis/internal/supabase"
+)
+
+func SubmitValidatedOutput(validatedDetails []*structOutputs.DetailAnalysisResult) (bool, error) {
+	for _, item := range validatedDetails {
+		switch item.Category {
+		case "CAPACITY":
+			if len(item.CapacityData.Capacities) > 0 {
+				supabase.StoreNewCapacity(item.CapacityData.Capacities)
+			}
+			if len(item.CapacityData.Units) > 0 {
+				supabase.StoreNewUnits(item.CapacityData.Units)
+			}
+			// TODO: as other cases are handled add more here
+		}
+	}
+
+	return true, nil
+}

1f6b1cc0a7962f0f57a58279559b4efdd0793db3
David Botos <dbotos@umich.edu>
2025-01-17 13:45:39 -0800

    need to upload new units before they can be referenced with new capacity objects

--- services/analysis/internal/processor/structOutputs/storeDetails.go
+++ services/analysis/internal/processor/structOutputs/storeDetails.go
@@ -7,19 +7,11 @@ import (
 	"github.com/david-botos/BearHug/services/analysis/pkg/logger"
 )
 
+// TODO: If the unit is new its uuid needs to be referenced by new capacity objects before those are uploaded...?
 func StoreDetails(extractedDetails []*DetailAnalysisResult, callID string) error {
 	log := logger.Get()
 	for _, detail := range extractedDetails {
 		if detail.Category == "CAPACITY" {
-			if len(detail.CapacityData.Capacities) > 0 {
-				capacityStorageErr := supabase.StoreNewCapacity(detail.CapacityData.Capacities, callID)
-				if capacityStorageErr != nil {
-					log.Error().
-						Err(capacityStorageErr).
-						Msg("Failed to store capacity details in supa")
-					return fmt.Errorf("error storing capacity details: %w", capacityStorageErr)
-				}
-			}
 			if len(detail.CapacityData.Units) > 0 {
 				unitStorageErr := supabase.StoreNewUnits(detail.CapacityData.Units, callID)
 				if unitStorageErr != nil {
@@ -27,6 +19,15 @@ func StoreDetails(extractedDetails []*DetailAnalysisResult, callID string) error
 						Err(unitStorageErr).
 						Msg("Failed to store unit details in supa")
 					return fmt.Errorf("error storing unit details: %w", unitStorageErr)
+				}
+			}
+			if len(detail.CapacityData.Capacities) > 0 {
+				capacityStorageErr := supabase.StoreNewCapacity(detail.CapacityData.Capacities, callID)
+				if capacityStorageErr != nil {
+					log.Error().
+						Err(capacityStorageErr).
+						Msg("Failed to store capacity details in supa")
+					return fmt.Errorf("error storing capacity details: %w", capacityStorageErr)
 				}
 			}
 		}

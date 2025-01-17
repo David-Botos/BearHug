c7527bdf5426fe6bdc621f8e481ba1c3223f2a77
David Botos <dbotos@umich.edu>
2025-01-17 13:43:35 -0800

    better logger

--- services/analysis/pkg/logger/logger.go
+++ services/analysis/pkg/logger/logger.go
@@ -22,9 +22,14 @@ func prettyPrint(i interface{}) string {
 		// Try to unmarshal and pretty print JSON bytes
 		var jsonData interface{}
 		if err := json.Unmarshal(v, &jsonData); err == nil {
-			if pretty, err := json.MarshalIndent(jsonData, "", "  "); err == nil {
-				return "\n" + string(pretty)
+			// Check if it's a complex type that needs pretty printing
+			if isComplexType(jsonData) {
+				if pretty, err := json.MarshalIndent(jsonData, "", "  "); err == nil {
+					return "\n" + string(pretty)
+				}
 			}
+			// For simple types, just return the string representation
+			return fmt.Sprint(jsonData)
 		}
 		// If not valid JSON, return as string
 		return string(v)
@@ -32,17 +37,34 @@ func prettyPrint(i interface{}) string {
 		// Try to unmarshal and pretty print JSON string
 		var jsonData interface{}
 		if err := json.Unmarshal([]byte(v), &jsonData); err == nil {
-			if pretty, err := json.MarshalIndent(jsonData, "", "  "); err == nil {
-				return "\n" + string(pretty)
+			// Check if it's a complex type that needs pretty printing
+			if isComplexType(jsonData) {
+				if pretty, err := json.MarshalIndent(jsonData, "", "  "); err == nil {
+					return "\n" + string(pretty)
+				}
 			}
+			// For simple types, just return the string representation
+			return fmt.Sprint(jsonData)
 		}
 		return v
 	default:
-		// For other types, try to marshal as JSON
-		if pretty, err := json.MarshalIndent(v, "", "  "); err == nil {
-			return "\n" + string(pretty)
+		// For other types, check if they need pretty printing
+		if isComplexType(v) {
+			if pretty, err := json.MarshalIndent(v, "", "  "); err == nil {
+				return "\n" + string(pretty)
+			}
 		}
-		return strings.ToUpper(fmt.Sprint(i))
+		return fmt.Sprint(v)
+	}
+}
+
+// isComplexType checks if the value is a complex type that needs pretty printing
+func isComplexType(v interface{}) bool {
+	switch v.(type) {
+	case map[string]interface{}, []interface{}, map[interface{}]interface{}:
+		return true
+	default:
+		return false
 	}
 }
 
@@ -69,12 +91,14 @@ func Init() {
 				}
 			},
 			FormatMessage: func(i interface{}) string {
-				return fmt.Sprintf("| %s |", i)
+				return fmt.Sprintf("| %s |\n", i)
 			},
 			FormatFieldName: func(i interface{}) string {
-				return fmt.Sprintf("%s:", i)
+				return fmt.Sprintf("%s: ", i)
 			},
-			FormatFieldValue: prettyPrint,
+			FormatFieldValue: func(i interface{}) string {
+				return fmt.Sprintf("%s", prettyPrint(i))
+			},
 		}
 
 		logger = zerolog.New(output).
